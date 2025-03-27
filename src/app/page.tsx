036fc1c0b772923cd110ad3bd0dbe806ef236277
nathnael getachew <nathnaelgetachew12f@gmail.com>
2025-03-27 20:30:50 +0300

    Initial setup with shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/darkmode";
 import Image from "next/image";
 
 export default function Home() {
@@ -98,6 +99,7 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+      <ModeToggle />
     </div>
   );
 }
